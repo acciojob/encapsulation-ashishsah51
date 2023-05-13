9f9e8824b7d71506bdf35c9955929643bd1d5ac6
ashishsah51 <ashishsah51@gmail.com>
2023-05-13 17:17:04 +0530

    All Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  // Main method
+    public static void main(String[] args) {
+      RWOnly obj = new RWOnly();
+      //   obj.name = "Ashish";
+      obj.setName("Ashish");
+      System.out.println(obj.getName());
+    }
 }
